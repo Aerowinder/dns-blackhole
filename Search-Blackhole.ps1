1390ade6608d739d69a872c988d1f8de6cfd1cf0
Aerowinder <51070353+Aerowinder@users.noreply.github.com>
2022-12-01 07:17:07 -0500

    Fixed broken code after directory change; fixed single return count

--- Search-Blackhole.ps1
+++ Search-Blackhole.ps1
@@ -1,9 +1,9 @@
-$base = [environment]::getfolderpath("mydocuments") + "\GitHub\dns-blackhole\"
-$dir_block = $base + 'block\'
+[string]$base = $PSScriptRoot
+$dir_txt = $base + '\txt\'
 
 $search = Read-Host -Prompt 'Input search term (wildcards implied)'
 $search = '*' + $search + '*'
-$blackhole = Get-Content -Path ($dir_block + "blackhole.txt") | Where-Object {$_ -Like $search}
+$blackhole = @(Get-Content -Path ($dir_txt + "block.txt") | Where-Object {$_ -Like $search}) # The @() forces the output to be an array. Previously, if one 1 result was returned, it would return as a string, and .Count would count the number of characters instead of total lines.
 
 Write-Host
 Write-Host 'Search Term:' $search '| Results:' $blackhole.Length -ForegroundColor Yellow
