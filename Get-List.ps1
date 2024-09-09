aa5293f315efb94cae92bbfaf29c183942d2bc91
Aerowinder <51070353+Aerowinder@users.noreply.github.com>
2024-09-09 08:45:15 -0400

    v2, more filtering added

--- Get-List.ps1
+++ Get-List.ps1
@@ -7,8 +7,17 @@ $hostlist = $dir_txt + 'host-list.txt'
 $blocklist = $dir_txt + 'block.txt'
 
 $regex_http = "^http:\/\/.*|^https:\/\/.*" # Allows comments and spacing in the host file list; ^http:\/\/.* = line starts with http:// | ^https:\/\/.* = line starts with https://
-$regex_line_cleanup = "^127\.0\.0\.1\s*|^0\.0\.0\.0\s*|\s*#.*$" # Lines that contain a domain, but contain other data as well; ^127\.0\.0\.1\s = line starts with '127.0.0.1 ' | ^0\.0\.0\.0\s = line starts with '0.0.0.0 ' | \s*#.*$ = inline comment
-$regex_line_junk = "^#|^\s*#|^\s*$|::" # Lines that do not contain a domain; ^# = line starts with # | ^\s*# = line starts with whitespace and goes until # (ie. '     #') | ^\s*$ = empty line | :: = literal (ie. IPv6 address)
+$regex_line_cleanup = "^127\.0\.0\.1\s*|^0\.0\.0\.0\s*|\s*#.*$|^\|\||\^$" # Lines that contain a domain, but contain other data as well. See below.
+    # ^127\.0\.0\.1\s = line starts with '127.0.0.1 '
+    # ^0\.0\.0\.0\s   = line starts with '0.0.0.0 '
+    # \s*#.*$         = inline comment ie. 'domain.com #domain comment'
+    # ^\|\|           = line that starts with '||' ie. '||domain.com' (https://v.firebog.net/hosts/RPiList-Malware.txt)
+    # \^$             = line that ends with '^' ie 'domain.com^' (https://v.firebog.net/hosts/RPiList-Malware.txt)
+$regex_line_junk = "^#|^\s*#|^\s*$|::" # Lines that do not contain a domain. See below.
+    # ^# = line starts with #
+    # ^\s*# = line starts with whitespace and goes until # (ie. '     #')
+    # ^\s*$ = empty line
+    # :: = literal (ie. IPv6 address)
 
 New-Item -Path $dir_txt_dl -ItemType Directory -Force | Out-Null
 
@@ -23,7 +32,7 @@ foreach ($entry in Get-Content -Path $hostlist) {
     Write-Host "Downloading $filename ..."
     Invoke-WebRequest -URI $entry -OutFile $filepath
 
-    Write-Host "Removing garbage from $filename ..."
+    Write-Host "Removing non-domain lines from $filename ..."
     switch ($filename) {
         "1.txt" {$list_content = Get-Content -Path $filepath | Select-Object -Skip 39 | Where-Object { $_ -NotMatch $regex_line_junk }} #Steven Black - contains non-commented data that will be difficult to filter later
         default {$list_content = Get-Content -Path $filepath | Where-Object { $_ -NotMatch $regex_line_junk }}
@@ -36,7 +45,7 @@ foreach ($entry in Get-Content -Path $hostlist) {
     $i += 1
 }
 
-Remove-Item -Path $dir_txt_dl -Recurse -Confirm:$false
+#Remove-Item -Path $dir_txt_dl -Recurse -Confirm:$false
 
 Write-Host "Performing final cleanup..."
 $list_combined = $list_combined  -Replace $regex_line_cleanup, '' | Sort-Object | Get-Unique
@@ -51,5 +60,8 @@ if ($git -eq 'add-commit-push') {
     git -C $base push origin main
 }
 
+Start-Sleep -Seconds 10 # Lazy troubleshooting. Gives me time to take a screenshot if there are any errors.
+
 #Changelog
 #2022-12-02 - AS - v1, First release.
+#2024-09-09 - AS - v2, Added more filtering for RPIMalware list (||, ^)
